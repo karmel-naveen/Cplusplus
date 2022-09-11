c7db6f677d3189a2c20cb9e5d0a1b76dbe1b90ca
Karmel-Naveen <naveenkarmel@gmail.com>
2022-09-11 09:32:29 +0530

    variables and basic data types

---
+++ basicdatatypes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    //variables and dataTypes
+    //integer
+    int x = 12;
+    int y = 12;
+    int sum = x+y;
+    std::cout << sum <<'\n';
+    
+    //Double (values with decimals)
+    double gpa =7.48;
+    double price = 10.99;
+    //char (single character)
+    char grade ='A';
+    char currency ='$';
+    //Boolean (true/false)
+    bool alive = true;
+    bool under_18 = false;
+    //String (an object which contains sequence of text)
+    std::string message = "Hello World";
+    std::cout << message;
+    return 0;
+}
