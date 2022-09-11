7b6be02c6e20e6decec32776edd2f546b5151841
Karmel-Naveen <naveenkarmel@gmail.com>
2022-09-11 09:07:25 +0530

    Initial commit

---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" <<'\n';
+    std::cout << "This is Karmel From planet Earth"<<'\n';
+    //This is a comment
+    std::cout << "Is anyone out there.?"<<'\n';
+    return 0;
+}
